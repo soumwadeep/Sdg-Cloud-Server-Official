8af6b4b1d5493e5c5be10c88ab155fbeb932e995
Soumwadeep Guha <soumwadeep@gmail.com>
2022-05-06 07:54:05 +0530

    added login

---
+++ login/js/login.js
@@ -0,0 +1,68 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-app.js";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword} from "https://www.gstatic.com/firebasejs/9.6.10/firebase-auth.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCToUfOtoyEnOZCu49oVDJy9YviIpBs8a8",
+    authDomain: "sdg-cloud-server-hub.firebaseapp.com",
+    projectId: "sdg-cloud-server-hub",
+    storageBucket: "sdg-cloud-server-hub.appspot.com",
+    messagingSenderId: "609514883727",
+    appId: "1:609514883727:web:ea725848990f295734befa"
+  };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth();
+
+// // Sign Up
+// signUp.addEventListener('click', (e) => {
+
+//   //Add All The Input Field's ID
+//   var email = document.getElementById("email").value;
+//   var password = document.getElementById("password").value;
+  
+//   //Copy It From Docs
+//   createUserWithEmailAndPassword(auth, email, password)
+//     .then((userCredential) => {
+//     // Signed in 
+//     const user = userCredential.user;
+//     alert("Welcome: " + user.email + "! ToSdg Cloud Server.You Are Now Successfully Registered!");
+//     window.location.href = "dashboard/home.html";
+//     // ...
+//     })
+//     .catch((error) => {
+//     const errorCode = error.code;
+//     const errorMessage = error.message;
+
+//   //add an alert for errors
+//   alert(errorMessage);
+
+//     // ..
+//     });
+// });
+  // Sign In
+signIn.addEventListener('click', (e) => {
+
+  var email = document.getElementById("email").value;
+  var password = document.getElementById("password").value;
+
+  signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+    // Signed in 
+    const user = userCredential.user;
+    alert("Welcome : " + user.email + "! Now You Can Access Our Files!");
+    window.location.href = "dashboard/home.html";
+    // ...
+    })
+    .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+
+  //add an alert for errors
+  alert(errorMessage);
+    });
+});
---
+++ login/js/validate.js
@@ -0,0 +1,48 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-app.js";
+import { getAuth, onAuthStateChanged , signOut } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-auth.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCToUfOtoyEnOZCu49oVDJy9YviIpBs8a8",
+    authDomain: "sdg-cloud-server-hub.firebaseapp.com",
+    projectId: "sdg-cloud-server-hub",
+    storageBucket: "sdg-cloud-server-hub.appspot.com",
+    messagingSenderId: "609514883727",
+    appId: "1:609514883727:web:ea725848990f295734befa"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth();
+
+const user = auth.currentUser;
+	onAuthStateChanged(auth, (user) => {
+  		if (user) {
+    	// User is signed in, see docs for a list of available properties
+    	// https://firebase.google.com/docs/reference/js/firebase.User
+    	const uid = user.uid;
+        // alert("Welcome: " + user.email + "! Now You Can Access Our Files!");
+    	// ...
+  		} else {
+    	// User is signed out
+            window.location.href = "../login.html";
+    	// ...
+  		}
+	});
+    // Sign Out
+    logOut.addEventListener('click', (e) => {
+        signOut(auth).then(() => {
+            // Sign-out successful.
+            alert("Bye! We Hope To Meet You Soon Again!");
+            window.location.href = "https://sdgcloudserver.com/";
+          }).catch((error) => {
+            // An error happened.
+            const errorCode = error.code;
+    	    const errorMessage = error.message;
+            //add an alert for errors
+		    alert(errorMessage);
+        });
+    });
